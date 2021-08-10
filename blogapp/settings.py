9feef42e7ea87ec64d1faa5581a547a24dd6ac67
PARTH2000-max <you@example.com>
2021-08-11 00:14:13 +0530

    save

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,5 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #manual add
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = "static"
+
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
